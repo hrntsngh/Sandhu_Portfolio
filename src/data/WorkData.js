7da7049090e97d01b833d2d8d0aedbfdd285f384
hrntsngh <harneetkahlon14@gmail.com>
2024-02-24 20:38:32 +0530

    .

---
+++ src/data/WorkData.js
@@ -0,0 +1,44 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"AIgirlfriend ",
+        description:"AIgirlfriend is a type of AI chatbot designed for conversation, specifically tailored to simulate a romantic partner. It utilizes advanced algorithms to provide an empathetic companion that you can communicate with at any time. The learns from your interactions, allowing it understand your emotions, thoughts, and preferences over time. This personalized connection aims to offer emotional support, enhance well-being, and cater to users seeking a virtual companion who understands and attentively listens to them.",
+
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id:2,
+        name:"Birthday Bliss",
+        description:"Birthday Bliss is a helpful app that manages birthdays and sends reminders for the special occasions of your loved ones. With the ability to add customized reminders, you can set hourly or up to unlimited times a day alerts, ensuring you never miss a birthday. The app allows you to create a list of people and their birthdays, making it easy to stay connected with friends and family. Birthday Bliss simplifies your life and strengthens your relationships by providing timely reminders and fostering a sense of appreciation for those close to you.",
+
+        
+        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"Semilla ",
+        description:"The Semilla app is a for fresh fruit andable delivery in Colombia connecting users with local farmers and cultivators. Users can browse a wide range of fresh,, and locally sour produce from nearby cultivators, place their orders, and make payments online. The app offers a user-friendly interface, making it easy to find and purchase the freshest fruits and vegetables. Semilla supports local communities by providing a platform for farmers to sell their produce directly to consumers, ensuring a fair price for both parties.",
+
+        
+        demo:"http://devto-clone-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/devto-clone"
+    },
+    {
+        id:4,
+        name:"Semilla Driver App",
+        description:"Semilla driver app: Facilitating the delivery fresh fruits and vegetables local cultivators to, providing a reliable income for drivers and promoting a sustainable food system. include a wage map for easy navigation and a rating system for quality assurance.",
+
+        
+        demo:"https://react-sidebar.vercel.app/",
+        github:"https://github.com/codebucks27/react-sidebar"
+       
+    },
+    
+  
+] 
